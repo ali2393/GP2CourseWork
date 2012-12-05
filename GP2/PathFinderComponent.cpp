d6a6680d3168bc60f9974fa7db31897da194ec76
Alisdair Muircroft <alisdairmuircroft@hotmail.com>
2012-12-05 16:37:08 +0000

    ?

---
+++ GP2/PathFinderComponent.cpp
@@ -0,0 +1,18 @@
+#pragma once
+
+#include "PathFinderComponent.h"
+
+CPathFinderComponent::CPathFinderComponent()
+{
+
+}
+
+CPathFinderComponent::~CPathFinderComponent()
+{
+
+}
+
+void CPathFinderComponent::update(float elapsedTime)
+{
+
+}
